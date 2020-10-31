810bd8de3b296a820d49f4fd1d34f7b0b8c196be
Ian Gregson <ian@iangregson.dev>
2020-10-31 08:54:53 +0100

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Ian Gregson on 31/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
